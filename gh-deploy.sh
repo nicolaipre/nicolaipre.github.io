604f7dd4638c3d3c2403da82f583b7831c5653f7
user <user@redacted.com>
2020-11-25 14:14:16 +0100

    Added example to gh-deploy.sh

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -2,6 +2,7 @@
 if [ -z "$1" ]
 then
   echo "Which folder do you want to deploy to GitHub Pages?"
+  echo "Example: ./gh-deploy.sh public"
   exit 1
 fi
 git subtree push --prefix $1 origin gh-pages
